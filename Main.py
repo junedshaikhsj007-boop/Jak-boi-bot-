213d31737b93e94f176b7a29a71cf2313f2ebf80
junedshaikhsj007-boop <junedshaikhsj.007@gmail.com>
2025-08-05 17:27:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -71,4 +71,17 @@ app.add_handler(CommandHandler("done", done))
 
 print("✅ Bot is running...")
 app.run_polling()
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
